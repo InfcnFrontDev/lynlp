57786d708f5986f640856dceb7143de8184ff858
zhangyan <459685710@qq.com>
2017-07-10 11:56:32 +0800

    00

--- src/demo/results/loading.jsx
+++ src/demo/results/loading.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactLoading from 'react-loading';
 export default class Loading extends React.Component {
 	render (){
 		return (
-			<div style={{width: 910,height: 100}}>
-				<ReactLoading type={'bars'} color={'#488fc1'} height='100' width='100' />
+			<div style={{width: 910,height: 100,textAlign: 'center'}}>
+				<img src={require('../../../images/loading.gif')} alt=""/>
 			</div>
 		)
 	}
