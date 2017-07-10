432a8fff7f8778ed1a02cf1af19eca00d77fb7e0
wangp <wangp@infcn.com.cn>
2017-07-10 16:31:17 +0800

    word-frequency

--- src/demo/results/word-frequency.jsx
+++ src/demo/results/word-frequency.jsx
@@ -1,177 +1,203 @@
 import React from "react";
-import axios from "axios";
+import WordSegmentationStore from "../../mobx/word-segmentation-store"
+import echarts from "echarts";
+import {observer} from "mobx-react";
+import _ from "lodash";
+
 
 /**
  * 词频统计
  */
+@observer
 export default class WordFrequency extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			content:'北京英富森软件股份有限公司是在北京市海淀区注册的高新技术企业、双软企业。“信息中国”（information china简称“infcn” ）是“英富森”的核心目标与战略。英富森公司的成立依托于凌云实验室的部分成果和理念，主要以信息管理与信息服务、知识管理与知识服务为基本方向，侧重于信息的整合、组织、发现和利用。通过先进的信息技术和服务理念，帮助行业客户建立企业级信息服务与知识服务平台，实现客户的企业级信息与知识的应用与发现。 公司来源于信息行业，依托于高校和科研院所，服务于行业客户。英富森凝聚了一支专业、高效、快乐、融洽的优秀团队，锻造出了一支服务型、管理型、创新型与开拓型的团队。',
+			graph:true,
+			list:false
+
+		};
+
+	}
+	componentWillMount(){
+		let content=this.state.content
+		WordSegmentationStore.fetchData('nlp',content,'北京')
+
+	}
+
+	groupArr(arr){
+		let nounG=_.groupBy(arr,function(n){return n.name});
+		let nounS=_.sortBy(nounG,'length');
+		let nounArr=[];
+		for(let i=nounS.length-1;i>=0;i--) {
+			nounArr.push(nounS[i]);
+		}
+		let nounA=nounArr.slice(0,10);
+		let nArr=[];
+		for(let i=0; i<nounA.length; i++){
+			nArr.push({
+				name:nounA[i][0].name,
+				length:nounA[i].length
+			})
+		}
+		return nArr;
+	}
+	graphShow(){
+		this.setState({
+			graph:true,
+			list:false
+		})
+
+	}
+	listShow(){
+		this.setState({
+			graph:false,
+			list:true
+		})
+	}
+	componentDidUpdate(){
+		let terms=WordSegmentationStore.nlp;
+
+		let cbb=_.groupBy(terms.$mobx.values,function(n){return n.natureStr.substr(0,1)});
+
+		let noun=cbb.n;
+		let verb=cbb.v;
+		let adjective=cbb.a;
+		let nArr=this.groupArr(noun);
+		let vArr=this.groupArr(verb);
+		let aArr=this.groupArr(adjective);
+		this.nounHtml=null;
+		this.verbHtml=null;
+		this.adjectiveHtml=null;
+		if(!this.state.graph){
+			this.chartHtml(nArr,'ming','#488fce')
+			this.chartHtml(vArr,'dong','#ff9900')
+			this.chartHtml(aArr,'xing','#67ccaa')
+
+
+		}
+	}
+	chartHtml(nArr,str,color){
+		let data1=[];
+		nArr.map(item=> data1.push(item.name));
+		let data2=[];
+		nArr.map(item=> data2.push(item.length));
+		console.log(document.getElementById(str))
+		data1=data1.reverse()
+		data2=data2.reverse()
+
+		var option = {
+
+
+			grid: {
+				left: '1%',
+				right: '3%',
+				bottom: '1%',
+				top:'1%',
+				containLabel: true
+			},
+			color:[color]
+			,
+			xAxis: {
+				type: 'value',
+				boundaryGap: [0, 1]
+			},
+			yAxis: {
+				type: 'category',
+				data:data1
+			},
+			series: [
+				{
+					type: 'bar',
+					data: data2
+				}
+
+			]
+		};
+
+		var myChart = echarts.init(document.getElementById(str));
+		myChart.setOption(option);
+	}
+	editHtml(nArr){
+		let ccc=nArr.map((item,index)=> <tr key={index}><td>{item.name}</td><td>{item.length}</td></tr>);
+		let nounHtml=(
+			<table className="cptab">
+				<tbody>
+				<tr>
+					<th>词名</th>
+					<th>词频</th>
+				</tr>
+				{ccc}
+				</tbody>
+			</table>
+		)
+		return nounHtml;
+	}
 
 
 	render() {
 		let {item} = this.props;
-		console.log(item);
-		return (
+		let terms=WordSegmentationStore.nlp;
+
+		let cbb=_.groupBy(terms.$mobx.values,function(n){return n.natureStr.substr(0,1)});
+		let noun=cbb.n;
+		let verb=cbb.v;
+		let adjective=cbb.a;
+		let nArr=this.groupArr(noun);
+		let vArr=this.groupArr(verb);
+		let aArr=this.groupArr(adjective);
+
+		if(this.state.graph){
+			this.nounHtml=this.editHtml(nArr);
+			this.verbHtml=this.editHtml(vArr);
+			this.adjectiveHtml=this.editHtml(aArr);
+		}
+
+
+
+
+
+			return (
 			<div className="m-hk">
 				<div className="jpt cf">
 					<h3 className="fl"><i>{item.title}</i></h3>
 					<div className="jftab fr" id="mr-3">
-						<span className="onsp">图形展示</span>
-						<span>列表展示</span>
+						<span className={this.state.graph?"onsp":''} onClick={()=>this.graphShow()}>图形展示</span>
+						<span className={this.state.list?"onsp":''} onClick={()=>this.listShow()}>列表展示</span>
 					</div>
 				</div>
 				<div className="cpm cf" style={{display:'block'}}>
 					<div className="cp-1 fl">
 						<h3 className="h3-1">名词</h3>
-						<table className="cptab">
-							<tbody>
-							<tr>
-								<th>词名</th>
-								<th>词频</th>
-							</tr>
-							<tr>
-								<td>大陆</td>
-								<td>22</td>
-							</tr>
-							<tr>
-								<td>民进党</td>
-								<td>33</td>
-							</tr>
-							<tr>
-								<td>英文</td>
-								<td>4</td>
-							</tr>
-							<tr>
-								<td>问题</td>
-								<td>5</td>
-							</tr>
-							<tr>
-								<td>九二共识</td>
-								<td>7</td>
-							</tr>
-							<tr>
-								<td>两岸</td>
-								<td>8</td>
-							</tr>
-							<tr>
-								<td>一国两制</td>
-								<td>9</td>
-							</tr>
-							<tr>
-								<td>共识</td>
-								<td>67</td>
-							</tr>
-							<tr>
-								<td>赖清德</td>
-								<td>54</td>
-							</tr>
-							<tr>
-								<td>魔术</td>
-								<td>44</td>
-							</tr>
-							</tbody>
-						</table>
+
+						{this.state.graph?this.nounHtml:(
+							<div style={{width:'100%',height:320}} id="ming">
+
+							</div>
+						)}
+
+
 					</div>
-					<div className="cp-2 fl">
+					<div className="cp-2 fl" >
 						<h3 className="h3-1">动词</h3>
-						<table className="cptab">
-							<tbody>
-							<tr>
-								<th>词名</th>
-								<th>词频</th>
-							</tr>
-							<tr>
-								<td>摘编</td>
-								<td>22</td>
-							</tr>
-							<tr>
-								<td>利用</td>
-								<td>33</td>
-							</tr>
-							<tr>
-								<td>接受</td>
-								<td>4</td>
-							</tr>
-							<tr>
-								<td>表示</td>
-								<td>5</td>
-							</tr>
-							<tr>
-								<td>废除</td>
-								<td>7</td>
-							</tr>
-							<tr>
-								<td>拆穿</td>
-								<td>8</td>
-							</tr>
-							<tr>
-								<td>不入流</td>
-								<td>9</td>
-							</tr>
-							<tr>
-								<td>有机可乘</td>
-								<td>67</td>
-							</tr>
-							<tr>
-								<td>就是</td>
-								<td>54</td>
-							</tr>
-							<tr>
-								<td>得到</td>
-								<td>44</td>
-							</tr>
-							</tbody>
-						</table>
+						{this.state.graph?this.verbHtml:(
+							<div id="dong" style={{width:'100%',height:320}}>
+
+							</div>
+						)}
+
+
 					</div>
 					<div className="cp-3 fl">
 						<h3 className="h3-1">形容词</h3>
-						<table className="cptab">
-							<tbody>
-							<tr>
-								<th>词名</th>
-								<th>词频</th>
-							</tr>
-							<tr>
-								<td>实际</td>
-								<td>22</td>
-							</tr>
-							<tr>
-								<td>空洞</td>
-								<td>33</td>
-							</tr>
-							<tr>
-								<td>心虚</td>
-								<td>4</td>
-							</tr>
-							<tr>
-								<td>深入</td>
-								<td>5</td>
-							</tr>
-							<tr>
-								<td>强硬</td>
-								<td>7</td>
-							</tr>
-							<tr>
-								<td>强大</td>
-								<td>8</td>
-							</tr>
-							<tr>
-								<td>繁荣</td>
-								<td>9</td>
-							</tr>
-							<tr>
-								<td>共识</td>
-								<td>67</td>
-							</tr>
-							<tr>
-								<td>赖清德</td>
-								<td>54</td>
-							</tr>
-							<tr>
-								<td>魔术</td>
-								<td>44</td>
-							</tr>
-							</tbody>
-						</table>
+						{this.state.graph?this.adjectiveHtml:(
+							<div style={{width:'100%',height:320}} id="xing">
+
+							</div>
+						)}
+
+
 					</div>
 				</div>
 				<div className="cpm cf">
@@ -191,4 +217,5 @@ export default class WordFrequency extends React.Component {
 			</div>
 		)
 	}
+
 }
