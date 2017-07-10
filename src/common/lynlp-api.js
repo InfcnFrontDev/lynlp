a2bf246ae88df68f55e17d546f418ba7e014c89a
杨可可 <yangkk@infcn.com.cn>
2017-07-10 10:10:25 +0800

    delete console

--- src/common/lynlp-api.js
+++ src/common/lynlp-api.js
@@ -1,8 +1,6 @@
 import config from "./config";
 
 const newPromise = function (url, params) {
-	console.log(params)
-
 	let formData = new FormData();
 	for (let key in params) {
 		formData.append(key, params[key]);
