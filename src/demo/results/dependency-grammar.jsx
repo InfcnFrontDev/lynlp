70cf1bda3f41e6d6ee1ac1c56fd7af02e0d1260a
Jason Ren <Jason@example.com>
2017-07-10 09:53:36 +0800

    .

--- src/demo/results/dependency-grammar.jsx
+++ src/demo/results/dependency-grammar.jsx
@@ -4,11 +4,23 @@ import React from 'react';
  * 依存文法
  */
 export default class DependencyGrammar extends React.Component {
+
 	render() {
+		let {item} = this.props;
 		return (
-			<div>
-				<h1>依存文法</h1>
+			<div className="m-hk">
+				<div className="jpt cf">
+					<h3 className="fl"><i>{item.title}</i></h3>
+				</div>
+				<div className="cf qfmkj">
+					<canvas id="myCanvas" width="890" height="300" style={{backgroundColor:'#ff0'}}></canvas>
+					<div>
+
+					</div>
+				</div>
 			</div>
 		)
 	}
 }
+
+
