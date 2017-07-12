f299b2e3d1095289bbb8995af823340d8c8c1145
chenxx <chenxx@infcn.com.cn>
2017-07-12 15:24:55 +0800

    .

--- src/demo/results/dependency-grammar.jsx
+++ src/demo/results/dependency-grammar.jsx
@@ -21,7 +21,7 @@ export default class DependencyGrammar extends React.Component {
 		var  parse = document.getElementById("parse");
 
 		if(parse){
-			parse.innerHTML = '<div id="tDiv"></div>';
+			parse.innerHTML = '<div id="tDiv" style="width: '+(docData.entities.length*80)+'px;"></div>';
 
 			var webFontURLs = [
 				'brat/static/fonts/Astloch-Bold.ttf',
@@ -50,8 +50,8 @@ export default class DependencyGrammar extends React.Component {
 					<h3 className="fl"><i>{item.title}</i></h3>
 				</div>
 				{isFetching?<Loading/>:(
-					<div className="cf qfmkj" id="parse">
-						<div id="tDiv"></div>
+					<div className="cf qfmkj" id="parse"  style={{width:890,overflowX:'scroll',overflowY:'hidden'}}>
+						<div id="tDiv" style={{width:4800}}></div>
 					</div>
 				)}
 			</div>
